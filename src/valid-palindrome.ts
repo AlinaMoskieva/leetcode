ab3e8258cd15de5fd08b2666a972c3c5337ed211
Alina Moskieva <alinamoskieva@gmail.com>
2023-08-24 18:18:41 +0200

    Valid Palindrome

---
+++ src/valid-palindrome.ts
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/valid-palindrome/
+// 125. Valid Palindrome
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+// Example 3:
+
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 2 * 105
+// s consists only of printable ASCII characters.
+
+function isPalindrome1(s: string): boolean {
+    const preparedString: string = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+    return preparedString === preparedString.split("").reverse().join("");
+};
+
+function isPalindrome(s: string): boolean {
+  const preparedString: string = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  for (let i = 0; i < preparedString.length / 2; i++) {
+    if (preparedString.charAt(i) !== preparedString.charAt(preparedString.length - i- 1)) return false;
+  }
+
+  return true;
+};
+
+console.log(isPalindrome("A man, a plan, a canal: Panama")) // true
+console.log(isPalindrome("race a car"))  // false
+console.log(isPalindrome(" ")) // true
+console.log(isPalindrome("ab_a")) // true
+console.log(isPalindrome("0P")) // false
