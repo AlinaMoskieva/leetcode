85f0a8f8496fcd4a69a7266e8c7759ecbc1809ea
Alina Moskieva <alinamoskieva@gmail.com>
2023-08-30 15:26:22 +0200

    Number of Good Pairs

---
+++ src/number-of-good-pairs.ts
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/number-of-good-pairs/
+// 1512. Number of Good Pairs
+// Given an array of integers nums, return the number of good pairs.
+
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+// Example 2:
+
+// Input: nums = [1,1,1,1]
+// Output: 6
+// Explanation: Each pair in the array are good.
+// Example 3:
+
+// Input: nums = [1,2,3]
+// Output: 0
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+
+function numIdenticalPairs(nums: number[]): number {
+  let numberOfPairs: number = 0;
+  let numberMaps: Map<number, number> = new Map();
+
+  nums.map((value) => numberMaps.set(value, numberMaps.has(value) ? numberMaps.get(value) + 1 : 1));
+
+  numberMaps.forEach((value) => numberOfPairs += value > 1 ? (value - 1) * value / 2 : 0);
+
+  return numberOfPairs;
+};
+
+console.log(numIdenticalPairs([1,2,3,1,1,3])); // 4
+console.log(numIdenticalPairs([1,1,1,1])); // 6
+console.log(numIdenticalPairs([1,2,3])); // 0
