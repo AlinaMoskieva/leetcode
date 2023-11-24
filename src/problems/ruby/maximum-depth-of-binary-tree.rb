9b4cf2a307af25ddb02eb62c26de428e1cdd04b1
Alina Moskieva <alinamoskieva@gmail.com>
2023-11-24 21:32:43 +0100

    Maximum Depth of Binary Tree

---
+++ src/problems/ruby/maximum-depth-of-binary-tree.rb
@@ -0,0 +1,53 @@
+# https://leetcode.com/problems/maximum-depth-of-binary-tree
+# 104. Maximum Depth of Binary Tree
+# Easy
+# Given the root of a binary tree, return its maximum depth.
+
+# A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+# Example 1:
+
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 3
+# Example 2:
+
+# Input: root = [1,null,2]
+# Output: 2
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 104].
+# -100 <= Node.val <= 100
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+#  Complexity: T: O(n), S: O(n)
+def max_depth(root)
+  return 0 if root.nil?
+
+  right_depth = 1 + max_depth(root.right)
+  left_depth = 1 + max_depth(root.left)
+
+  [right_depth, left_depth].max
+end
